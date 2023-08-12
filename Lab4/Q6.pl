1f623f3c853c63702ee6cab78e24f3c8b52dec8f
Daniel <dne33@uclive.ac.nz>
2023-08-12 16:23:23 +1200

    Did Q6

---
+++ Lab4/Q6.pl
@@ -0,0 +1,20 @@
+directlyIn(Wrapper,Inner) :- Wrapper=olga, Inner=katarina; Wrapper=natasha, Inner=olga; Wrapper=irina, Inner=natasha.
+contains(Wrapper,Inner) :- directlyIn(Inner,Wrapper); directlyIn(Z,Wrapper), contains(Z,Inner).
+
+
+
+test_answer :-
+    directlyIn(irina, natasha),
+    writeln('OK').
+
+test_answer2 :-
+    \+ directlyIn(irina, olga),
+    writeln('OK').
+
+test_answer3 :-
+    contains(katarina, irina),
+    writeln('OK').
+test_answer4 :-
+    findall(P, contains(P, irina), Output),
+    sort(Output, SortedOutput),
+    foreach(member(X,SortedOutput), (write(X), nl)).
